45aa1545fe34024748c58c5396843a8cbf3a58f8
fabian <fabian.salas.astete@gmail.com>
2023-12-04 16:47:16 -0300

    issues: updates of design section carousel and new button wsp contact

--- src/components/PageSections/About/index.js
+++ src/components/PageSections/About/index.js
@@ -12,9 +12,9 @@ const AboutComponent = () => {
   return (
     <Section>
     {/* <Fade delay={300} direction="right"> */}
-    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  gap-4">
-      <div className="col-span-1 md:col-span-1 xl:col-span-2 flex flex-col justify-center mx-2 xl:ml-24">
-        <h2 className="text-6xl xl:text-5xl text-center  text-primary font-bold">
+    <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-3  gap-1">
+      <div className="col-span-1 md:col-span-1 xl:col-span-2 flex flex-col justify-center xl:ml-6">
+        <h2 className="text-6xl xl:text-5xl text-center text-primary font-bold">
             Sobre mí
           </h2>
         <div className='grid grid-cols-1 xl:grid-cols-2'>
@@ -30,7 +30,7 @@ const AboutComponent = () => {
             :null}
         </div>
         <div className="col-span-2 xl:col-span-1 flex flex-col">
-          <p className="text-lg xl:text-xl text-gray-700 mt-8 ml-0 xl:ml-5 md:text-center  xl:text-left">
+          <p className="text-lg xl:text-xl text-gray-700 mt-8 ml-0 xl:ml-1 xl:text-left">
           Soy un asesor profesional inmobiliario con especialización en la gestión de administración de propiedades. Mi compromiso radica en brindar un servicio de calidad y excelencia a mis clientes. Mi área de trabajo abarca la región metropolitana de Santiago, donde gestiono una cartera diversificada de propiedades.
 
             <br />
@@ -44,10 +44,10 @@ const AboutComponent = () => {
             <br /> */}
           </p>
 
-          <p className="text-lg xl:text-xl text-gray-700 mt-8 ml-0 xl:ml-5 md:text-center xl:text-left">
+          <p className="text-lg xl:text-xl text-gray-700 mt-8 ml-0 xl:ml-1 xl:text-left">
             Danny Ortiz.
           </p>
-          <p className="text-xl xl:text-xl text-gray-700 ml-0 xl:ml-5 md:text-center xl:text-left">
+          <p className="text-xl xl:text-xl text-gray-700 ml-0 xl:ml-1 xl:text-left">
             Fundador y Gerente General.
           </p>
         </div>
